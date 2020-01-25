2183a55e217f4b6894b6820db2b1472650e6835f
DOLA <mo.adel@nu.edu.eg>
2020-01-25 15:29:42 +0200

    Authantication the admin user name and password to add users into db

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port        = 3050;
 const MongoClient = require('mongodb').MongoClient;
 const url         = "mongodb://localhost:27017/nodejsDB";
 var bodyParser = require('body-parser')
+var dataa = [];
+var userStatus = null;
 //communcating with express to use bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -14,34 +16,26 @@ app.use(bodyParser.urlencoded({
 var ddb = null;
 
 //add data to db
-function addData(res) {
-  //create array that holds data
-  var dataa = [
-    {name : "TEST", password:"Test"},
-    {name : "TEST1", password:"Test1"},
-    {name : "TEST2", password:"Test2"}
-  ];
+function addData(dataa,res) {
 ddb.collection("userss").insertMany(dataa,function(err){
   if (err) throw err;
+  console.log(dataa[0].name); 
+  res.send(dataa[0].name +" added to Database");
 });
 }
 //function to test given data to the stored data in the db
 function compare(username,password,res){
-  //init user status
   var userStatus = null;
-  //but data in query format to search in db
   var query = {name : username , password : password};
-  //access db to search for the assigned data
   ddb.collection("userss").find(query).toArray(function(err,result){
     if (err) {throw err;};
-    //checking for the data serached is it existed or not
     if(typeof result[0] !=='undefined'){
-    userStatus = 'Accepted';
-    res.send('Welcome => ' +username);}
+    res.send('Welcome => ' +username);
+  }
     else{
-      userStatus = 'Rejected';
-      res.send('No data fond');
-    }
+      userStatus = "Rejected";
+      res.send("This data dosn't exist");
+      }
   });
 }
 app.post('/',function(req,res){
@@ -50,9 +44,21 @@ MongoClient.connect(url,function(err,db){
         ddb = db.db("nodejsDB");
         username = req.body.username;
         password = req.body.password;
-        console.log(req.body);
-        addData(res);
+        addTodb = req.body.addTodb;
+        adminUser = req.body.adminUser;
+        adminPassword = req.body.adminPassword
+
+        if(addTodb =='1'){
+          if(adminUser == 'Admin' && adminPassword =='Admin'){
+          addData([{name :username ,password:password}],res);
+        }else{
+          res.send("Invalid username or password")
+        }
+        }
+        else{
         compare(username,password,res);
+        }
+        console.log(req.body);
         db.close();
       });
 });
